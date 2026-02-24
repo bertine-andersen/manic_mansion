c983bdc34525cd486abb32f87063a21bc4baedb9
maje0401 <146211476+maje0401@users.noreply.github.com>
2026-02-24 13:45:28 +0100

    klasse layout

---
+++ klasser.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+import pygame as pg
+from konstanter import *
+from dataclasses import dataclass
+
+@dataclass(slots=True)
+class SpillBrett:
+    # Områder på spillbrettet
+    frisone1: pg.Rect
+    frisone2: pg.Rect
+    fareSone: pg.Rect
+
+    # Sauer plukket opp
+    poeng: int
+
+    def oppstart(self):
+        pass
+
+    def nyHindring(self):
+        pass
+
+    def nySau(self):
+        pass
+
+    def nyttSpøkelse(self):
+        pass
+
+@dataclass(slots=True)
+class Spiller:
+    # Områder på spillbrettet
+    rect: pg.Rect
+
+    levende: bool = True
+    harSau: bool = False
+
+    def plukkOpp(self):
+        pass
+
+    def treffSpøkelse(self):
+        pass
+
+    def hindring(self):
+        pass
+
+    def kant(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+@dataclass(slots=True)
+class Sau:
+    rect: pg.Rect
+    plukketOpp: bool
+
+@dataclass(slots=True)
+class Spøkelse:
+    rect: pg.Rect
+    
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+@dataclass(slots=True)
+class Hindring:
+    rect: pg.Rect
